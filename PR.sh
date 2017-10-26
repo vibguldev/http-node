2cc13452530626d521d4b2122bf99ad418248756
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-10-26 09:17:05 +0530

    PR

--- PR.sh
+++ PR.sh
@@ -1,1 +1,1 @@
-git checkout master && git checkout -b $1 && touch .$2 && git add . && git commit -m  '100 keep' && git push origin $1 && git checkout master
+git checkout master && git checkout -b $1 && echo 'trying ...' >> README.md && git add . && git commit -m 'README update' && git push origin $1 && git checkout master
